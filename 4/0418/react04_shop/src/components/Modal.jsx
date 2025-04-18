0671abda27140cc263712cf5bfaac5c26aa78d9a
Lacheln <psionict@naver.com>
2025-04-18 11:43:35 +0900

    모달 구조 제작

--- 4/0418/react04_shop/src/components/Modal.jsx
+++ 4/0418/react04_shop/src/components/Modal.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import css from './Modal.module.css';
 
 const Modal = () => {
-  return <div></div>;
+  return (
+    <div className={css.modal}>
+      <div className={css.container}>
+        <div className={css._inner}>
+          컨텐츠가 들어가는 곳<h2>장바구니</h2>
+          <div className={css.imgWrap}>
+            <img src="/public/img/image1.jpg" alt="" />
+          </div>
+          <div className={css.info}>
+            <p>상품명</p>
+            <p>가격</p>
+            <p>할인률</p>
+            <p>수량</p>
+          </div>
+          <button>취소</button>
+          <button>장바구니 담기</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
