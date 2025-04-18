87cf185327c04575f987a3adfc5f12b8ed829b2a
Lacheln <psionict@naver.com>
2025-04-18 10:06:37 +0900

    기본 구조

--- 4/0418/react04_shop/src/organism/DetailTabInfo.jsx
+++ 4/0418/react04_shop/src/organism/DetailTabInfo.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
 
 const DetailTabInfo = () => {
-  return <div></div>
+  return (
+    <>
+      <div className={css.tabBtn}>
+        <button>메뉴1</button>
+        <button>메뉴2</button>
+        <button>메뉴3</button>
+      </div>
+      <div className={css.tabContent}>
+        <h3>제목</h3>
+        <p>내용</p>
+        <p>내용</p>
+        <p>내용</p>
+        <p>내용</p>
+        <p>내용</p>
+      </div>
+    </>
+  )
 }
 
 export default DetailTabInfo
