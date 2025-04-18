0b8a7b84d861c1019abd5527770847dbdcddb2f5
Lacheln <psionict@naver.com>
2025-04-18 10:25:12 +0900

    tab관리 1

--- 4/0418/react04_shop/src/organism/DetailTabInfo.jsx
+++ 4/0418/react04_shop/src/organism/DetailTabInfo.jsx
@@ -1,16 +1,43 @@
-import React from 'react'
-import css from './DetailTabInfo.module.css'
+import React, { useState } from 'react';
+import css from './DetailTabInfo.module.css';
 
 const DetailTabInfo = () => {
+  // n번째 탭을 useState로 관리
+  const [activeTab, setActive] = useState(0);
+  const tabTiles = ['메뉴1', '메뉴2', '메뉴3'];
+
   return (
     <>
       <div className={css.tabBtn}>
+        {tabTiles.map((title, i) => (
+          <button key={i} className={css.active} onClick={() => setActive(i)}>
+            {title}
+          </button>
+        ))}
         <button className={css.active}>메뉴1</button>
         <button>메뉴2</button>
         <button>메뉴3</button>
       </div>
       <div className={css.tabContent}>
-        <h3>제목</h3>
+        <h3>제목1</h3>
+        <p>내용</p>
+        <p>내용</p>
+        <p>내용</p>
+        <p>내용</p>
+        <p>내용</p>
+      </div>
+
+      <div className={css.tabContent}>
+        <h3>제목2</h3>
+        <p>내용</p>
+        <p>내용</p>
+        <p>내용</p>
+        <p>내용</p>
+        <p>내용</p>
+      </div>
+
+      <div className={css.tabContent}>
+        <h3>제목3</h3>
         <p>내용</p>
         <p>내용</p>
         <p>내용</p>
@@ -18,7 +45,7 @@ const DetailTabInfo = () => {
         <p>내용</p>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default DetailTabInfo
+export default DetailTabInfo;
