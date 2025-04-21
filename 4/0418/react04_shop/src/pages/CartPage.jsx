ca5a4eb4a56ebfd2fe625f0f74e6ebc926c0337d
Lacheln <psionict@naver.com>
2025-04-21 09:30:15 +0900

    장바구니 페이지 구조 만들기

--- 4/0418/react04_shop/src/pages/CartPage.jsx
+++ 4/0418/react04_shop/src/pages/CartPage.jsx
@@ -7,6 +7,31 @@ const CartPage = () => {
   return (
     <main>
       <h2>CartPage</h2>
+      <p>you have 3 item</p>
+      <ul>
+        <li>
+          <div>
+            <img src="/public/img/image1.jpg" alt="cart" />
+          </div>
+          <div>상품명</div>
+          <div>가격</div>
+          <div>
+            <button>-</button>
+            <span>1</span>
+            <button>+</button>
+          </div>
+          <div>합계</div>
+          <div>
+            <i className="bi bi-trash3"></i>
+          </div>
+          <div>
+            <button>삭제</button>
+          </div>
+        </li>
+      </ul>
+      <div>
+        총금액 : <strong>100,000원</strong>
+      </div>
     </main>
   );
 };
