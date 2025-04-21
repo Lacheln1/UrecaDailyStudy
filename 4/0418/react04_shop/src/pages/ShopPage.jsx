d5517201e91151d19219abcb1794468c117829ba
Lacheln <psionict@naver.com>
2025-04-21 14:11:14 +0900

    shopPage 구조 작성

--- 4/0418/react04_shop/src/pages/ShopPage.jsx
+++ 4/0418/react04_shop/src/pages/ShopPage.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from 'react';
 
 const ShopPage = () => {
   return (
     <main>
       <h2>ShopPage</h2>
+      <div>
+        {/* 카테고리 선택 기능 new,top,기본  */}
+        <div>
+          <button>전체상품</button>
+          <button>신상품(new)</button>
+          <button>인기상품(top)</button>
+        </div>
+        {/* {정렬기능} */}
+        <div>
+          <div>등록순</div>
+          <ul>
+            <li>등록순</li>
+            <li>낮은 가격순</li>
+            <li>높은 가격순</li>
+            <li>낮은 할인순</li>
+            <li>높은 할인순</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <ul>
+          <li>상품리스트</li>
+        </ul>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default ShopPage
+export default ShopPage;
