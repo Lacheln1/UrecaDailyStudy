c85c6e8e694e0480c08a0f25539f0406755d4f5d
Lacheln <psionict@naver.com>
2025-04-21 09:33:53 +0900

    css모듈 클래스 부여

--- 4/0418/react04_shop/src/pages/CartPage.jsx
+++ 4/0418/react04_shop/src/pages/CartPage.jsx
@@ -9,27 +9,24 @@ const CartPage = () => {
       <h2>CartPage</h2>
       <p>you have 3 item</p>
       <ul className={css.cartList}>
-        <li>
-          <div>
+        <li className={css.cartItem}>
+          <div className={css.cartImg}>
             <img src="/public/img/image1.jpg" alt="cart" />
           </div>
-          <div>상품명</div>
-          <div>가격</div>
-          <div>
+          <div className={css.title}>상품명</div>
+          <div className={css.price}>가격</div>
+          <div className={css.btnArea}>
             <button>-</button>
             <span>1</span>
             <button>+</button>
           </div>
-          <div>합계</div>
-          <div>
+          <div className={css.sum}>합계</div>
+          <div className={css.deleteBtn}>
             <i className="bi bi-trash3"></i>
-          </div>
-          <div>
-            <button>삭제</button>
           </div>
         </li>
       </ul>
-      <div>
+      <div className={css.totalPrice}>
         총금액 : <strong>100,000원</strong>
       </div>
     </main>
