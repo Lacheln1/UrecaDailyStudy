16a5105e58d56f5f336134357fc62b2cc04f408d
Lacheln <psionict@naver.com>
2025-04-22 14:32:56 +0900

    페이지 번호 기능 구현

--- 4/0418/react04_shop/src/components/Pagination.jsx
+++ 4/0418/react04_shop/src/components/Pagination.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import css from './Pagination.module.css';
 import { useNavigate, useSearchParams } from 'react-router-dom';
+import { max } from 'lodash';
 
 const Pagination = ({ initProductsData }) => {
   //데이터가 어떻게 나오는지 확인하기
@@ -17,6 +18,31 @@ const Pagination = ({ initProductsData }) => {
     params.set('_page', page); //페이지를 1로 초기화
     navigate(`/shop/?${page}`);
   };
+
+  //페이지 번호 계산 함수
+  const getPageNumber = () => {
+    //한번에 보여줄 최대 페이지 번호 수
+    const maxPageNumbers = 10;
+    //전체 페이지가 최대 페이지보다 작으면 모든 페이지번호 표시
+    if (pages > maxPageNumbers) {
+      return Array.from({ length: maxPageNumbers }, (_, i) => i + 1);
+    }
+
+    //페이지가 많을 경우 현재 페이지 번호를 기준으로 주변 번호 생성
+    // 예)현재 페이지 15=> 10~20까지 보여줌
+    let startPage = Math.max(1, currentPage - Math.floor(maxPageNumbers / 2));
+    let endPage = Math.min(pages, startPage + maxPageNumbers - 1);
+
+    //endPage가 pages보다 작을 경우 startPage조정
+    if (endPage > pages) {
+      endPage = pages;
+      startPage = Math.max(1, endPage - maxPageNumbers + 1);
+    }
+    return Array.from({ length: endPage - startPage + 1 }, (_, i) => startPage + i);
+  };
+
+  const pageNumbers = getPageNumber();
+  console.log('pageNumbers', pageNumbers);
 
   return (
     <div className={css.paginationArea}>
