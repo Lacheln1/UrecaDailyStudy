aa0bd53f1409203db71ea5bbaba703237fe3ed4d
Lacheln <psionict@naver.com>
2025-04-22 14:22:12 +0900

    pagination 버튼 이벤트

--- 4/0418/react04_shop/src/components/Pagination.jsx
+++ 4/0418/react04_shop/src/components/Pagination.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import css from './Pagination.module.css';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 const Pagination = ({ initProductsData }) => {
-  console.log('last', initProductsData.products.last);
+  //데이터가 어떻게 나오는지 확인하기
+  console.log('======', initProductsData);
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+  const { first, last, prev, nex, pages, items } = initProductsData.products;
+
+  const currentPage = searchParams.get('_page') || 1;
+  console.log('currentPage == ', currentPage);
+
+  const handlePageChange = page => {
+    const params = new URLSearchParams(searchParams); // 현재 파라미터 정보 유지
+    params.set('_page', page); //페이지를 1로 초기화
+    navigate(`/shop/?${page}`);
+  };
+
   return (
     <div className={css.paginationArea}>
-      <button>
+      <button
+        onClick={() => {
+          handlePageChange(first);
+        }}
+        disabled={currentPage === first}
+        className={currentPage === first ? css.disabled : ''}
+      >
         <i className="bi bi-chevron-left"></i>
       </button>
       <button>1</button>
