c72d97a490b5842bedb5c0f42f33d9be07c5ee5d
Lacheln <psionict@naver.com>
2025-04-22 16:05:01 +0900

    pagination수정

--- 4/0418/react04_shop/src/components/Pagination.jsx
+++ 4/0418/react04_shop/src/components/Pagination.jsx
@@ -1,30 +1,27 @@
 import React from 'react';
 import css from './Pagination.module.css';
 import { useNavigate, useSearchParams } from 'react-router-dom';
-import { max } from 'lodash';
 
 const Pagination = ({ initProductsData }) => {
   //데이터가 어떻게 나오는지 확인하기
   console.log('======', initProductsData);
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
-  const { first, last, prev, nex, pages, items } = initProductsData.products;
 
-  const currentPage = Number(searchParams.get('_page') || 1);
-  console.log('currentPage == ', currentPage);
+  const { first, last, prev, next, pages } = initProductsData.products;
+  const currentPage = Number(searchParams.get('_page') || '1');
 
   const handlePageChange = page => {
-    const params = new URLSearchParams(searchParams); // 현재 파라미터 정보 유지
-    params.set('_page', page); //페이지를 1로 초기화
-    navigate(`/shop/?${page}`);
+    const params = new URLSearchParams(searchParams);
+    params.set('_page', page);
+    navigate(`/Shop/?${params}`);
   };
 
   //페이지 번호 계산 함수
-  const getPageNumber = () => {
     //한번에 보여줄 최대 페이지 번호 수
+  const getPageNumbers = () => {
     const maxPageNumbers = 10;
-    //전체 페이지가 최대 페이지보다 작으면 모든 페이지번호 표시
-    if (pages > maxPageNumbers) {
+    if (pages <= maxPageNumbers) {
       return Array.from({ length: maxPageNumbers }, (_, i) => i + 1);
     }
 
@@ -41,8 +38,7 @@ const Pagination = ({ initProductsData }) => {
     return Array.from({ length: endPage - startPage + 1 }, (_, i) => startPage + i);
   };
 
-  const pageNumbers = getPageNumber();
-  console.log('pageNumbers', pageNumbers);
+  const pageNumbers = getPageNumbers();
 
   return (
     <div className={css.paginationArea}>
@@ -53,6 +49,15 @@ const Pagination = ({ initProductsData }) => {
         disabled={currentPage === first}
         className={currentPage === first ? css.disabled : ''}
       >
+        처음으로이동
+      </button>
+      <button
+        onClick={() => {
+          handlePageChange(prev);
+        }}
+        disabled={prev === null || currentPage === first}
+        className={currentPage === first ? css.disabled : ''}
+      >
         <i className="bi bi-chevron-left"></i>
       </button>
       {pageNumbers.map(num => (
@@ -61,6 +66,7 @@ const Pagination = ({ initProductsData }) => {
           onClick={() => {
             handlePageChange(num);
           }}
+          className={num === currentPage ? `${css.active}` : ''}
         >
           {num}
         </button>
@@ -73,6 +79,14 @@ const Pagination = ({ initProductsData }) => {
         className={currentPage === last ? css.disabled : ''}
       >
         <i className="bi bi-chevron-right"></i>
+      </button>
+      <button
+        onClick={() => {
+          handlePageChange(last);
+        }}
+        disabled={next === null || currentPage === last}
+      >
+        마지막 페이지 이동
       </button>
     </div>
   );
