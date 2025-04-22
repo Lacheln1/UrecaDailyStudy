6889068c81c2f8dffc425b99588b52e67c999668
Lacheln <psionict@naver.com>
2025-04-22 10:41:17 +0900

    pagianation구조 작성

--- 4/0418/react04_shop/src/components/Pagination.jsx
+++ 4/0418/react04_shop/src/components/Pagination.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import css from './Pagination.module.css';
 
-const Pagination = () => {
-  return <div></div>;
+const Pagination = ({ data }) => {
+  return (
+    <div className={css.paginationArea}>
+      <button>
+        <i className="bi bi-chevron-left"></i>
+      </button>
+      <button>1</button>
+      <button>2</button>
+      <button className={css.active}>3</button>
+      <button>4</button>
+      <button>5</button>
+      <button>
+        <i className="bi bi-chevron-right"></i>
+      </button>
+    </div>
+  );
 };
 
 export default Pagination;
--- 4/0418/react04_shop/src/pages/ShopPage.jsx
+++ 4/0418/react04_shop/src/pages/ShopPage.jsx
@@ -39,22 +39,9 @@ const ShopPage = () => {
       <div className={css.productList}>
         <ul className={css.list}>
           {data.map(product => (
-            <ProductCard key={product.id} data={data} />
+            <ProductCard key={product.id} data={product} />
           ))}
         </ul>
-        <div className={css.paginationArea}>
-          <button>
-            <i className="bi bi-chevron-left"></i>
-          </button>
-          <button>1</button>
-          <button>2</button>
-          <button className={css.active}>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>
-            <i className="bi bi-chevron-right"></i>
-          </button>
-        </div>
       </div>
     </main>
   );
