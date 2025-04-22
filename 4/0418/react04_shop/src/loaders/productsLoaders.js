e987313454fb8d6619462d534fe30bc67b78ec1d
Lacheln <psionict@naver.com>
2025-04-22 11:50:08 +0900

    productLoader작성

--- 4/0418/react04_shop/src/loaders/productsLoaders.js
+++ 4/0418/react04_shop/src/loaders/productsLoaders.js
@@ -33,18 +33,30 @@ export const detailPageLoader = async info => {
 };
 
 export const shopPageLoader = async ({ request }) => {
-  // console.log('productLoader====', request.url);
+  // console.log('productsLoaders.js:info', request.url)
   const url = new URL(request.url);
   const page = url.searchParams.get('_page') || 1;
   const per_page = url.searchParams.get('_per_page') || 12;
   const category = url.searchParams.get('category') || '';
+  const sort = url.searchParams.get('_sort') || '';
+  // const per_page = 12
+  let queryString = `_page=${page}&_per_page=${per_page}`;
+  category ? (queryString += `&category=${category}`) : queryString;
+  sort ? (queryString += `&_sort=${sort}`) : queryString;
 
   //_page=2&_per_page=2    :  데이터를 다 가져오지 않고 필요한 데이터만을 가져오기 위한 작업
+  console.log('-----------------', queryString);
 
-  let queryString = `?_page=${page}&_per_page=${per_page}`;
-  queryString ? (queryString += `&category=${category}`) : queryString;
+  try {
+    const products = await getProductsData(queryString);
+    console.log('productsLoaders.js:products ----- ', products);
 
-  const products = await getProductsData(queryString);
   //하나만 리턴하면 그냥 쭉 나오는데 두가지 이상을 return을 하면 console에서 per_page, products라는 key가 된다
-  return { products, per_page };
+    return { products, per_page };
+  } catch (err) {
+    console.log('err----', err);
+    throw new Response('상품 데이터를 가져오는 중 오류 발생', {
+      status: err.status || 500,
+    });
+  }
 };
