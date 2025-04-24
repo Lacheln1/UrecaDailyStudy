e64c1018a551fcde964a5c2a110b82c4d4cc4853
Lacheln <psionict@naver.com>
2025-04-24 10:15:05 +0900

    useSelector 사용해보기

--- 4/0418/react04_shop/src/pages/BlogPage.jsx
+++ 4/0418/react04_shop/src/pages/BlogPage.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 const BlogPage = () => {
+  //counter 은 slice이름임
+  const countData = useSelector(state => state.counter);
+  const { count, label } = countData;
+  console.log('countdata는====', countData);
+
   return (
     <main>
       <h2>BlogPage</h2>
+      <p>
+        {label} : {count}
+      </p>
+      <button>증가하기</button>
     </main>
-  )
-}
+  );
+};
 
-export default BlogPage
+export default BlogPage;
