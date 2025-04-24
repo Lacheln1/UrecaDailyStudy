3894a7561862c2257ef9bb3b0e541968e07aa008
Lacheln <psionict@naver.com>
2025-04-24 11:32:51 +0900

    다크모드 토글 구현하기

--- 4/0418/react04_shop/src/organism/Header.jsx
+++ 4/0418/react04_shop/src/organism/Header.jsx
@@ -3,6 +3,7 @@ import { Link, NavLink, useLocation } from 'react-router-dom';
 import css from './Header.module.css';
 import Logo from '../components/Logo';
 import { throttle } from '@/utils/features';
+import { isDate } from 'lodash';
 
 const Header = () => {
   const [isOn, setIsOn] = useState(false);
@@ -29,6 +30,26 @@ const Header = () => {
     };
   }, [handleResize]);
 
+  //다크모드 토글 구현하기
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const handleThemeToggle = () => {
+    useEffect(() => {
+      const savedTheme = localStorage.getItem('theme');
+      if (savedTheme !== null) {
+        const parsedTheme = JSON.parse(savedTheme);
+        setIsDarkMode(parsedTheme);
+        //toggle클래스는 자바스크립트 기본 문법으로 두번째 매개변수에 따라 true or false 조작
+        document.body.classList.toggle('dark-mode', parsedTheme);
+      }
+    }, []);
+    const handleThemeToggle = () => {
+      const newTheme = !isDarkMode;
+      setIsDarkMode(newTheme);
+      //localstorage가져올땐 parse, set할땐 stringfy필수 : 문자열만 들어갈수있기때문
+      localStorage.setItem('theme', JSON.stringify(newTheme));
+    };
+  };
+
   return (
     <header className={css.hd}>
       <div className={css.con}>
@@ -47,6 +68,11 @@ const Header = () => {
             <CustomIconLink to={'/search'} icon={'bi-search'} />
             <CustomIconLink to={'/mypage'} icon={'bi-person-circle'} />
             <CustomIconLink to={'/cart'} icon={'bi-basket'} />
+            <i
+              className={`p-2 bi bi-${isDarkMode ? 'moon' : 'sun'}`}
+              style={{ cursor: 'pointer' }}
+              onClick={handleThemeToggle}
+            ></i>
           </div>
         </div>
         <i className={`${css.ham} bi bi-list`} title="전체메뉴 보기버튼" onClick={addClassOn}></i>
