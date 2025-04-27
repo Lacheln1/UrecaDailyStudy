2fbbc0f4be2b3a74119c6e13ab9cfb4d9484e536
Lacheln <psionict@naver.com>
2025-04-28 01:28:45 +0900

    input 여러개 다루기

--- 4/0427/practice/EventPractice.jsx
+++ 4/0427/practice/EventPractice.jsx
@@ -2,14 +2,20 @@ import React, { useState } from "react";
 
 const EventPractice = () => {
   const [message, setMessage] = useState("");
+  const [name, setName] = useState("");
 
-  const handleChange = (e) => {
+  const handleMessageChange = (e) => {
     setMessage(e.target.value);
   };
 
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
   const clickEvent = () => {
-    alert(message);
+    alert(name + ":" + message);
     setMessage("");
+    setName("");
   };
 
   return (
@@ -20,9 +26,15 @@ const EventPractice = () => {
         name="message"
         placeholder="아무거나 입력"
         value={message}
-        onChange={handleChange}
+        onChange={handleMessageChange}
       />
-
+      <input
+        type="text"
+        name="name"
+        placeholder="사용자명"
+        value={name}
+        onChange={handleNameChange}
+      />
       <button onClick={clickEvent}>확인하기</button>
     </div>
   );
