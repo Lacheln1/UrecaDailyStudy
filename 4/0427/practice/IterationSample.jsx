2376300cfc5795dc61fe758f4196d96686f4aa42
Lacheln <psionict@naver.com>
2025-04-29 02:19:58 +0900

    데이터 추가 기능
    onChange로 setText해준 값을
    onClick에서 concat(새로운 배열을 만들어줌) 으로 setText해준 값을 넣어 새로운 배열 생성후 setNames하여 추가

--- 4/0427/practice/IterationSample.jsx
+++ 4/0427/practice/IterationSample.jsx
@@ -11,9 +11,22 @@ const IterationSample = () => {
   const [inputText, setInputText] = useState("");
   const [nextId, setNextId] = useState(5); //새로운 항목을 추가할 때 사용할 id
 
+  const onChange = (e) => setInputText(e.target.value);
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId, //nextId값을 id로 설정하고
+      text: inputText,
+    });
+    setNextId(nextId + 1); // nextId값에 1을 더해 준다
+    setNames(nextNames); // names값을 업데이트한다
+    setInputText(""); // inputText를 비운다
+  };
+
   const namesList = names.map((name) => <li key={name.id}>{name.text}</li>);
   return (
     <div>
+      <input type="text" value={inputText} onChange={onChange} />
+      <button onClick={onClick}>추가</button>
       <ul>{namesList}</ul>
     </div>
   );
