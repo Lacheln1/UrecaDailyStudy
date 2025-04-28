07e9f9db6c890daf78d2546f0336991fef78b72e
Lacheln <psionict@naver.com>
2025-04-28 10:29:43 +0900

    메인 레이아웃 제작

---
+++ 4/0428/react05_apis/src/layout/MainLayout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MainLayout = () => {
+  return (
+    <>
+      <ul>
+        <li>리스트</li>
+      </ul>
+      <main>컨텐츠 들어가는 곳</main>
+    </>
+  );
+};
+
+export default MainLayout;
