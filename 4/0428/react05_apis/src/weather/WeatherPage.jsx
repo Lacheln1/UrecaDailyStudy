adeb7095f73bdfa60085ac4e6964b3c6cfd91c35
Lacheln <psionict@naver.com>
2025-04-28 11:14:57 +0900

    웨더페이지 구조 작성

--- 4/0428/react05_apis/src/weather/WeatherPage.jsx
+++ 4/0428/react05_apis/src/weather/WeatherPage.jsx
@@ -4,7 +4,16 @@ const WeatherPage = () => {
   return (
     <main>
       <h2>weatherPage</h2>
-      <div>컨텐츠영역</div>
+      <div>
+        <p>국가 / 도시 </p>
+        <p>온도 / icon</p>
+      </div>
+      <div>
+        <button>리스트</button>
+        <button>리스트</button>
+        <button>리스트</button>
+        <button>리스트</button>
+      </div>
     </main>
   );
 };
