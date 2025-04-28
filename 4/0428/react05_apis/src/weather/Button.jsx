1f1df06b04b5268f6657a967db8fd04c971b4069
Lacheln <psionict@naver.com>
2025-04-28 15:18:04 +0900

    css수정

--- 4/0428/react05_apis/src/weather/Button.jsx
+++ 4/0428/react05_apis/src/weather/Button.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 const Button = ({ city, label, onClick }) => {
   return (
-    <button id={city} onClick={() => onClick(city)}>
+    <button
+      id={city}
+      onClick={() => onClick(city)}
+      style={{ padding: "1rem", border: "1px solid gray" }}
+    >
       {label}
     </button>
   );
