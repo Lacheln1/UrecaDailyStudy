5e8631c5cbb78e08d3afefd017b4b0ab7fb8744e
Lacheln <psionict@naver.com>
2025-04-28 11:42:36 +0900

    웨더get

--- 4/0428/react05_apis/src/weather/useWeatherApi.js
+++ 4/0428/react05_apis/src/weather/useWeatherApi.js
@@ -1,2 +1,11 @@
 //https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-// https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=d7bf29a51b222bb8ddfd1b62b05c21f4
+const testUrl =
+  "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=d7bf29a51b222bb8ddfd1b62b05c21f4";
+
+import axios from "axios";
+
+export const getCurrentData = async () => {
+  const res = await axios.get(testUrl);
+  console.log(res);
+  return res.data;
+};
