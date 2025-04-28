0d3570e51a5db0290f0266a8846f31aea0ed7c9d
Lacheln <psionict@naver.com>
2025-04-28 14:14:42 +0900

    데이터 바인딩하기

--- 4/0428/react05_apis/src/weather/WeatherPage.jsx
+++ 4/0428/react05_apis/src/weather/WeatherPage.jsx
@@ -1,25 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import css from "./WeatherPage.module.css";
 import { getCurrentData } from "./useWeatherApi";
 
 const WeatherPage = () => {
+  const [weatherData, setWeatherData] = useState(null);
   useEffect(() => {
     const fetchWeatherData = async () => {
       try {
         const data = await getCurrentData();
         console.log("날씨 데이터", data);
+        setWeatherData(data);
       } catch (error) {
         console.error("날씨 데이터 가져오기 실패", error);
       }
     };
     fetchWeatherData();
   }, []);
+  console.log("날시데이터", weatherData?.cod);
   return (
     <main className={css.main}>
       <h2>weatherPage</h2>
       <div className={css.weatherInfo}>
-        <p className={css.location}>국가 / 도시 </p>
-        <p className={css.temperature}>온도 / icon</p>
+        <p className={css.location}>
+          {weatherData?.sys.country} / {weatherData?.name}{" "}
+        </p>
+        <p className={css.temperature}>
+          {weatherData?.main.temp} &#8451; /{" "}
+          {weatherData?.weather[0].description}
+        </p>
       </div>
       <div className={css.btnList}>
         <button>리스트</button>
