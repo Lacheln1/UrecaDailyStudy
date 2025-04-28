ed41f0517ad52158625b32bf020b99d9367b66d7
Lacheln <psionict@naver.com>
2025-04-28 10:30:41 +0900

    메뉴 리스트 제작

--- 4/0428/react05_apis/src/layout/MainLayout.jsx
+++ 4/0428/react05_apis/src/layout/MainLayout.jsx
@@ -3,9 +3,6 @@ import React from "react";
 const MainLayout = () => {
   return (
     <>
-      <ul>
-        <li>리스트</li>
-      </ul>
       <main>컨텐츠 들어가는 곳</main>
     </>
   );
---
+++ 4/0428/react05_apis/src/layout/MenuList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MenuList = () => {
+  return (
+    <ul>
+      <li>리스트</li>
+      <li>리스트</li>
+      <li>리스트</li>
+      <li>리스트</li>
+    </ul>
+  );
+};
+
+export default MenuList;
