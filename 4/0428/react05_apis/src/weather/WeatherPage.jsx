83250f1e751e8beed26e48641cb4a8dbc23e02b3
Lacheln <psionict@naver.com>
2025-04-28 10:42:52 +0900

    웨더페이지 제작

---
+++ 4/0428/react05_apis/src/weather/WeatherPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const WeatherPage = () => {
+  return (
+    <main>
+      <h2>weatherPage</h2>
+      <div></div>
+      <div>컨텐츠영역</div>
+    </main>
+  );
+};
+
+export default WeatherPage;
