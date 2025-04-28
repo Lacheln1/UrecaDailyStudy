4a216fb9f1e0d61a78358e8adf11423d976b336d
Lacheln <psionict@naver.com>
2025-04-28 13:29:53 +0900

    env활용 1

--- 4/0428/react05_apis/src/weather/useWeatherApi.js
+++ 4/0428/react05_apis/src/weather/useWeatherApi.js
@@ -1,11 +1,13 @@
-//https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-const testUrl =
-  "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=d7bf29a51b222bb8ddfd1b62b05c21f4";
+//https://api.openweathermap.org/data/2.5/weather?lat=44&lon=10&appid=bcd8d4f55126e2e478ac4efd6462ead2&lang=kr&units=metric
 
 import axios from "axios";
 
+const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 export const getCurrentData = async () => {
-  const res = await axios.get(testUrl);
+  const res = await axios.get(
+    `${BASE_URL}?lat=44&lon=10&appid=${API_KEY}&lang=kr&units=metric`
+  );
   console.log(res);
   return res.data;
 };
