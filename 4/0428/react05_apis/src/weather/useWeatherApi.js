ed5dff3eb8dbaa52a0dae82813a13c6a851a7dfd
Lacheln <psionict@naver.com>
2025-04-28 11:40:41 +0900

    웨더api.js제작

---
+++ 4/0428/react05_apis/src/weather/useWeatherApi.js
@@ -0,0 +1,2 @@
+//https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+// https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=d7bf29a51b222bb8ddfd1b62b05c21f4
