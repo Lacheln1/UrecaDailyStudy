1cf0066b13dd9468a4d8d83b0797b242129e01bd
Lacheln <psionict@naver.com>
2025-04-29 10:44:07 +0900

    캠핑api활용해보기

---
+++ 4/0428/react05_apis/src/camping/CampingPage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const CampingPage = () => {
+  return (
+    <main>
+      <h2>캠핑페이지</h2>
+      <div>
+        <p>총 nnnn개 중 n개 표시 / 현재 1페이지</p>
+        <ul>
+          <li>
+            <p>야영장명</p>
+            <p>주소</p>
+            <p>연락처</p>
+          </li>
+        </ul>
+      </div>
+    </main>
+  );
+};
+
+export default CampingPage;
