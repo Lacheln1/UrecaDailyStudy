079d28e4b26a939cb2e58347a58ebdf822e8c8af
Lacheln <psionict@naver.com>
2025-04-29 11:33:11 +0900

    map오류 수정
    {campingData?.map
    
    data?.data로 campingData를 만들고 있는데
    
    data나 data.data가 아직 없을 때 map()을 호출하려고 해서 에러가 터진 것
    
    저렇게 작성하여 해결

--- 4/0428/react05_apis/src/camping/CampingPage.jsx
+++ 4/0428/react05_apis/src/camping/CampingPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useCamping } from "./useCamping";
+import css from "./CampingPage.module.css";
 
 const CampingPage = () => {
   const { data, isError, isLoading } = useCamping(1, 10);
@@ -7,18 +8,24 @@ const CampingPage = () => {
   const totalCount = campingData?.totalCount;
   const page = campingData?.page;
   const perPage = campingData?.perPage;
-  console.log("캠핑데이터=====", data);
+  console.log("캠핑데이터=====", data?.data);
+  const handleCampingClick = (e) => {};
+
+  isLoading && <p>로딩중..</p>;
+  isError && <p>에러</p>;
   return (
     <main>
       <h2>캠핑페이지</h2>
       <div>
         <p>총 nnnn개 중 n개 표시 / 현재 1페이지</p>
-        <ul>
-          <li>
-            <p>야영장명</p>
-            <p>주소</p>
-            <p>연락처</p>
-          </li>
+        <ul className={css.list}>
+          {campingData?.map((list, i) => (
+            //id가 따로 없고 한글명으로 구별해야할땐 . 으로 접근 못함 , i해준 이유는 야영장명이 같은곳이 있을 수 있으니까
+            <li key={list["야영장명"] + i}>
+              <p>야영장명 : {list["야영장명"]}</p>
+              <p>주소 : {list["주소"]}</p>
+            </li>
+          ))}
         </ul>
       </div>
     </main>
