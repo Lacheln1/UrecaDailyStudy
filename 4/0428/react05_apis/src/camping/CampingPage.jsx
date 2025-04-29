282f49feb3cf75f11e370a1797989fd01d797df6
Lacheln <psionict@naver.com>
2025-04-29 16:50:06 +0900

    캠핑페이지-페이지네이션 연결

--- 4/0428/react05_apis/src/camping/CampingPage.jsx
+++ 4/0428/react05_apis/src/camping/CampingPage.jsx
@@ -1,35 +1,37 @@
 import React, { useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import { useCamping } from "./useCamping";
 import css from "./CampingPage.module.css";
 import DetailModal from "./DetailModal";
 import Pagination from "./Pagination";
 
 const CampingPage = () => {
+  const [searchParams] = useSearchParams();
   const [isModalOpen, setIsModalOpen] = useState(false);
   //선택된 캠핑 정보들을 모달에다가 전달해주기 위함
   const [selected, setSelected] = useState(null);
-  const [currentPage, setCurrentPage] = useState(1);
 
-  //1대신에 페이지 넘길때마다 버튼올라가면서 하기?
+  const currentPage = Number(searchParams.get("_page") || "1");
+
   const { data, isError, isLoading } = useCamping(currentPage, 10);
-  const campingData = data?.data;
-  const totalCount = campingData?.totalCount;
-  const page = campingData?.page;
-  const perPage = campingData?.perPage;
-  console.log("캠핑page=====", data?.data);
+  const campingData = data?.data; // 실제 캠핑 배열
+  const totalCount = data?.totalCount;
+  const page = data?.page;
+  const perPage = data?.perPage;
 
   const handleCampingClick = (list) => {
     setIsModalOpen(true);
     setSelected(list);
   };
 
-  isLoading && <p>로딩중..</p>;
-  isError && <p>에러</p>;
   return (
     <main>
       <h2>캠핑페이지</h2>
       <div>
-        <p>총 nnnn개 중 n개 표시 / 현재 1페이지</p>
+        <p>
+          총 {totalCount}개 중 {campingData?.length || 0}개 표시 / 현재 {page}
+          페이지
+        </p>
         <ul className={css.list}>
           {campingData?.map((list, i) => (
             //id가 따로 없고 한글명으로 구별해야할땐 . 으로 접근 못함 , i해준 이유는 야영장명이 같은곳이 있을 수 있으니까
@@ -43,6 +45,7 @@ const CampingPage = () => {
           ))}
         </ul>
       </div>
+
       {isModalOpen && <DetailModal selected={selected} />}
       <Pagination data={data} />
     </main>
