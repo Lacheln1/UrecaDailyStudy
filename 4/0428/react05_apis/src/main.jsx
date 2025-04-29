4258fb57c484596bef84b729e54dd37bc90dfcaf
Lacheln <psionict@naver.com>
2025-04-29 09:42:50 +0900

    tanstack 쿼리 실습
    맨 처음 해야할것

--- 4/0428/react05_apis/src/main.jsx
+++ 4/0428/react05_apis/src/main.jsx
@@ -4,9 +4,13 @@ import "./assets/index.css";
 import MainLayout from "./layout/MainLayout.jsx";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/index.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 );
