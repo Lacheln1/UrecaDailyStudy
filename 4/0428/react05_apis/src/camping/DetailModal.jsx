82dc39ebad406f39b7a77570c8f3b58ef54cf44a
Lacheln <psionict@naver.com>
2025-04-29 13:31:13 +0900

    modal 닫기 구현

--- 4/0428/react05_apis/src/camping/DetailModal.jsx
+++ 4/0428/react05_apis/src/camping/DetailModal.jsx
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import css from "./DetailModal.module.css";
+import CampingPage from "./CampingPage";
 const DetailModal = ({ selected }) => {
-  console.log("selected===", selected);
+  const [isModalClose, setIsModalClose] = useState(false);
+  const closeModal = () => {
+    setIsModalClose(true);
+    console.log("클릭됨");
+  };
+
+  useEffect(() => {
+    const fetchModalState = () => {
+      setIsModalClose(false);
+    };
+    fetchModalState();
+  }, [selected]);
+
+  if (isModalClose) return null;
 
   return (
-    <div className={css.modal}>
-      <div className={css.modalContent}>
-        <h2>상세정보</h2>
-        <p>{selected["야영장명"]}</p>
-        <p>{selected["시군구"]}</p>
-        <p>{selected["주소"]}</p>
-        <p>{selected["인허가일자"]}</p>
-        <button>닫기</button>
+    <>
+      <div className={css.modal}>
+        <div className={css.modalContent}>
+          <h2>상세정보</h2>
+          <p>{selected["야영장명"]}</p>
+          <p>{selected["시군구"]}</p>
+          <p>{selected["주소"]}</p>
+          <p>{selected["인허가일자"]}</p>
+          <button onClick={closeModal}>닫기</button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
