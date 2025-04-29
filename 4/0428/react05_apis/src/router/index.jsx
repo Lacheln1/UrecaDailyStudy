358f87850038b9091943814769d2e282549bbb08
Lacheln <psionict@naver.com>
2025-04-29 10:44:51 +0900

    router연결하기

--- 4/0428/react05_apis/src/router/index.jsx
+++ 4/0428/react05_apis/src/router/index.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 
 import WeatherPage from "../weather/WeatherPage";
 import MainLayout from "../layout/MainLayout";
+import CampingPage from "../camping/CampingPage";
 
 export const router = createBrowserRouter([
   {
@@ -12,6 +13,10 @@ export const router = createBrowserRouter([
       {
         index: true,
         element: <WeatherPage />,
+      },
+      {
+        path: "/camping",
+        element: <CampingPage />,
       },
     ],
   },
