887f2a3a3219e74957d1bd7aa4fd5a2d6a0ec650
Lacheln <psionict@naver.com>
2025-04-29 09:45:10 +0900

    데브툴 활성화

--- 4/0428/react05_apis/src/main.jsx
+++ 4/0428/react05_apis/src/main.jsx
@@ -5,12 +5,15 @@ import MainLayout from "./layout/MainLayout.jsx";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/index.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      {/* 개발환경에서만 데브툴이 나오게 하기 */}
+      {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
     </QueryClientProvider>
   </StrictMode>
 );
