f9e38d69fcae237bcfd534534c808dc6fb617008
Lacheln <psionict@naver.com>
2025-04-30 13:33:34 +0900

    유효성 검사 및 회원가입 api호출 기초세팅

--- 4/0430/reactblog06/frontend/src/pages/RegisterPage.jsx
+++ 4/0430/reactblog06/frontend/src/pages/RegisterPage.jsx
@@ -8,6 +8,7 @@ const RegisterPage = () => {
   const [errorPassWord, setErrorPassWord] = useState("");
   // 패스워드 확인
   const [errorPassWordOk, setErrorPassWordOk] = useState("");
+  const [registerState, setRegisterState] = useState("");
 
   //유효성 검사
   const validdateUsername = (value) => {
@@ -25,7 +26,31 @@ const RegisterPage = () => {
     }
   };
 
-  const validatePassword = (value) => {};
+  const validatePassword = (value) => {
+    if (!value) {
+      setErrorPassWord("");
+      return;
+    }
+    if (value.length < 4) {
+      setErrorPassWord("패스워드는 4자 이상이어야 합니다");
+    } else {
+      setErrorPassWord("");
+    }
+  };
+
+  const validatePasswordCheck = (value, current = passWord) => {
+    if (!value) {
+      setErrorPassWordOk("");
+      return;
+    }
+    if (value !== current) {
+      setErrorPassWordOk("패스워드가 일치하지 않습니다");
+    } else {
+      setErrorPassWordOk("");
+    }
+  };
+
+  // onChange함수들
   const handleUserNameChange = (e) => {
     const value = e.target.value;
     setUserName(value);
@@ -36,10 +61,43 @@ const RegisterPage = () => {
     setPassWord(value);
     validatePassword(value);
   };
+
+  const handleUserPassWordOkChange = (e) => {
+    const value = e.target.value;
+    setPassWordOk(value);
+    validatePasswordCheck(value);
+  };
+
+  //폼 제출전 폼값이 변경됐을수도있으니 한번 더 유효성 검사
+  const register = (e) => {
+    e.preventDefault();
+    console.log("회원가입", userName, passWord, passWordOk);
+    validdateUsername(userName);
+    validatePassword(passWord);
+    validatePasswordCheck(passWordOk, passWord);
+
+    if (
+      errorUserName ||
+      errorPassWord ||
+      errorPassWordOk ||
+      !userName ||
+      !passWord ||
+      !passWordOk
+    ) {
+      return;
+    }
+
+    //회원가입 api 호출
+    try {
+      setRegisterState("등록중");
+    } catch (error) {
+      console.log("회원가입 실패", error);
+    }
+  };
   return (
     <main className={css.registerpage}>
       <h2>회원가입 페이지</h2>
-      <form className={css.container}>
+      <form className={css.container} onSubmit={register}>
         <input
           type="text"
           placeholder="사용자명"
@@ -54,7 +112,12 @@ const RegisterPage = () => {
           onChange={handleUserPassWordChange}
         />
         <strong>{errorPassWord}</strong>
-        <input type="password" placeholder="패스워드 확인" value={passWordOk} />
+        <input
+          type="password"
+          placeholder="패스워드 확인"
+          value={passWordOk}
+          onChange={handleUserPassWordOkChange}
+        />
         <strong>{errorPassWordOk}</strong>
         <button type="submit">가입하기</button>
       </form>
