456a8cea40c20645125f4f1e1e10bbb167d44c64
Lacheln <psionict@naver.com>
2025-04-30 11:33:28 +0900

    회원가입 폼 구조짜기

--- 4/0430/reactblog06/frontend/src/pages/RegisterPage.jsx
+++ 4/0430/reactblog06/frontend/src/pages/RegisterPage.jsx
@@ -1,10 +1,19 @@
 import React from "react";
-
+import css from "./RegisterPage.module.css";
 const RegisterPage = () => {
   return (
-    <div>
-      <p>회원가입 페이지</p>
-    </div>
+    <main className={css.registerpage}>
+      <h2>회원가입 페이지</h2>
+      <form>
+        <input type="text" placeholder="사용자명" />
+        <strong>검사결과</strong>
+        <input type="password" placeholder="패스워드" />
+        <strong>검사결과</strong>
+        <input type="password" placeholder="패스워드 확인" />
+        <strong>검사결과</strong>
+        <button type="submit">가입하기</button>
+      </form>
+    </main>
   );
 };
 
