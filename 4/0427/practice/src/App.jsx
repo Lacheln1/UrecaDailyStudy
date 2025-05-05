b791042f9d00fb7897554571cdd49bd378ca168e
Lacheln <psionict@naver.com>
2025-05-05 21:52:30 +0900

    useRef 로컬 변수 사용하기
    로컬 변수 : 렌더링과 상관없이 바뀔 수 있는 값
    
    이렇게 작성하면 ref안의 값이 바뀌어도 컴포넌트가 렌더링 되지 않는다

--- 4/0427/practice/src/App.jsx
+++ 4/0427/practice/src/App.jsx
@@ -5,11 +5,12 @@ import "./App.css";
 import Counter from "./practice/Counter";
 import Info from "./practice/Info";
 import Average from "./practice/Average";
+import RefSample from "./practice/RefSample";
 
 function App() {
   return (
     <>
-      <Average />
+      <RefSample />
     </>
   );
 }
