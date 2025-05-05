936e0253c51ec5f7d15bbd45bb0130cb32e8c143
Lacheln <psionict@naver.com>
2025-05-05 18:43:51 +0900

    useReducer 실습2
    이런식으로 input을 관리하면 input의 개수가 많아져도 코드를 비교적 짧고 깔금하게 유지할 수 있다

--- 4/0427/practice/src/App.jsx
+++ 4/0427/practice/src/App.jsx
@@ -3,11 +3,12 @@ import { useState } from "react";
 import "./App.css";
 
 import Counter from "./practice/Counter";
+import Info from "./practice/Info";
 
 function App() {
   return (
     <>
-      <Counter />
+      <Info />
     </>
   );
 }
---
+++ 4/0427/practice/src/practice/Info.jsx
@@ -0,0 +1,44 @@
+import { useReducer } from "react";
+import React from "react";
+
+const reducer = (state, action) => {
+  return {
+    ...state,
+    [action.name]: action.value,
+  };
+};
+
+const Info = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    name: "",
+    nickname: "",
+  });
+
+  const { name, nickname } = state;
+  const onChange = (e) => {
+    dispatch(e.target);
+  };
+  return (
+    <div>
+      <div>
+        <input type="text" name="name" value={name} onChange={onChange} />
+        <input
+          type="text"
+          name="nickname"
+          value={nickname}
+          onChange={onChange}
+        />
+      </div>
+      <div>
+        <div>
+          <b>이름 :</b> {name}
+        </div>
+      </div>
+      <div>
+        <b>닉네임 : </b> {nickname}
+      </div>
+    </div>
+  );
+};
+
+export default Info;
