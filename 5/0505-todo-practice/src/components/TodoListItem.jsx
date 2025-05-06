c1e5d5921424bb0d9c0d82d6461fa05dbc5ab64d
Lacheln <psionict@naver.com>
2025-05-06 21:39:39 +0900

    list아이템 스타일이 깨지는 문제 발생
    props로 받아 온 style을 적용시켜주면 됨

--- 5/0505-todo-practice/src/components/TodoListItem.jsx
+++ 5/0505-todo-practice/src/components/TodoListItem.jsx
@@ -5,19 +5,25 @@ import {
   MdCheckBoxOutlineBlank,
   MdRemoveCircleOutline,
 } from "react-icons/md";
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
   const { id, text, checked } = todo;
   return (
-    <div className={css.todoListItem}>
-      <div
-        className={`${css.checkBox} ${checked ? css.checked : ""}`}
-        onClick={() => onToggle(id)}
-      >
-        {checked ? <MdCheckBox color="#22b8cf" /> : <MdCheckBoxOutlineBlank />}
-        <div className={css.text}>{text}</div>
-      </div>
-      <div className={css.remove} onClick={() => onRemove(id)}>
-        <MdRemoveCircleOutline />
+    <div className={css.TodoListItemVirtualized} style={style}>
+      <div className={css.todoListItem}>
+        <div
+          className={`${css.checkBox} ${checked ? css.checked : ""}`}
+          onClick={() => onToggle(id)}
+        >
+          {checked ? (
+            <MdCheckBox color="#22b8cf" />
+          ) : (
+            <MdCheckBoxOutlineBlank />
+          )}
+          <div className={css.text}>{text}</div>
+        </div>
+        <div className={css.remove} onClick={() => onRemove(id)}>
+          <MdRemoveCircleOutline />
+        </div>
       </div>
     </div>
   );
