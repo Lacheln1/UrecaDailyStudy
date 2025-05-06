7052d342c80852dc052b291ab60cb640f2e8d570
Lacheln <psionict@naver.com>
2025-05-07 02:49:49 +0900

    중첩된 라우트 실습
    1. app.jsx의 route를 감싸준다
    2. articles.js에 outlet컴포넌트를 사용한다 (컴포넌트가 보여져야 하는 부분에 사용)

--- 5/0506-router-tutorial/src/App.jsx
+++ 5/0506-router-tutorial/src/App.jsx
@@ -5,15 +5,19 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Articles from "./pages/Articles";
 import Article from "./pages/Article";
+import Layout from "./Layout";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="about" element={<About />} />
-      <Route path="/profiles/:username" element={<Profile />} />
-      <Route path="/articles" element={<Articles />} />
-      <Route path="/articles/:id" element={<Article />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="/profiles/:username" element={<Profile />} />
+      </Route>
+      <Route path="/articles" element={<Articles />}>
+        <Route path=":id" element={<Article />} />
+      </Route>
     </Routes>
   );
 };
