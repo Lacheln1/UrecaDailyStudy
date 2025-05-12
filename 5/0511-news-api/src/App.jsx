5e1fe815cc2036b7037d267bdb599130021a556b
Lacheln <psionict@naver.com>
2025-05-13 02:00:33 +0900

    뉴스api실습을 위한 구조 작성

--- 5/0511-news-api/src/App.jsx
+++ 5/0511-news-api/src/App.jsx
@@ -1,32 +1,10 @@
-import axios from "axios";
-import React, { useState } from "react";
+import React from "react";
+import NewsList from "./components/NewsList";
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        "https://newsapi.org/v2/top-headlines?country=kr&apiKey=5b14f02f7eb846729e6ae260b4c5b634"
-      );
-      setData(response.data);
-      console.log(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
     <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
+      <NewsList />
     </div>
   );
 };
