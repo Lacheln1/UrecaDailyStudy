dfe17ae00c764e9e189551a210b2e8fa7c405029
Lacheln <psionict@naver.com>
2025-05-28 01:51:15 +0900

    redux2

---
+++ 5/0519-redux-practice/src/modules/todos.js
@@ -0,0 +1,29 @@
+const CHANGE_INPUT = "todos/CHANGE_INPUT"; // 인풋 값 변경
+const INSERT = "todos/INSERT"; // 새로운 TODO를 등록함
+const TOGGLE = "todos/TOGGLE"; //TODO를 체크/체크 해제함
+const REMOVE = "todos/REMOVE"; //TODO를 제거함
+
+export const changeInput = (input) => ({
+    type: CHANGE_INPUT,
+    input,
+});
+
+let id = 3;
+export const insert = (text) => ({
+    type: INSERT,
+    todo: {
+        id: id++,
+        text,
+        done: false,
+    },
+});
+
+export const toggle = (id) => ({
+    type: TOGGLE,
+    id,
+});
+
+export const remove = (id) => ({
+    type: REMOVE,
+    id,
+});
