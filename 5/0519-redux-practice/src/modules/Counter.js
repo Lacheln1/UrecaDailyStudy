47b39566432d14afd1511c31a1c550a8838d2bf4
Lacheln <psionict@naver.com>
2025-05-28 01:16:28 +0900

    react redux

---
+++ 5/0519-redux-practice/src/modules/Counter.js
@@ -0,0 +1,5 @@
+const INCREASE = "Counter/INCREASE";
+const DECREASE = "Counter/DECREASE";
+
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
