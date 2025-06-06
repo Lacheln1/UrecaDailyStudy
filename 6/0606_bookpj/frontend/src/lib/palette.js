4794348b69959ad61fc06845b788b72041a9bae0
Lacheln <psionict@naver.com>
2025-06-07 01:52:04 +0900

    버튼 컴포넌트 만들기

---
+++ 6/0606_bookpj/frontend/src/lib/palette.js
@@ -0,0 +1,23 @@
+:root {
+    --gray-0: #f8f9fa;
+    --gray-1: #f1f3f5;
+    --gray-2: #e9ecef;
+    --gray-3: #dee2e6;
+    --gray-4: #ced4da;
+    --gray-5: #adb5bd;
+    --gray-6: #868e96;
+    --gray-7: #495057;
+    --gray-8: #343a40;
+    --gray-9: #212529;
+
+    --cyan-0: #e3fafc;
+    --cyan-1: #c5f6fa;
+    --cyan-2: #99e9f2;
+    --cyan-3: #66d9e8;
+    --cyan-4: #3bc9db;
+    --cyan-5: #22b8cf;
+    --cyan-6: #15aabf;
+    --cyan-7: #1098ad;
+    --cyan-8: #0c8599;
+    --cyan-9: #0b7285;
+}
