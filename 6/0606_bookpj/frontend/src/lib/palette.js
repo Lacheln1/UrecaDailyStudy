7715cdbe537a1ad3dc8658b18ab2f29183aa82b7
Lacheln <psionict@naver.com>
2025-06-07 02:05:05 +0900

    indexcss제작

--- 6/0606_bookpj/frontend/src/lib/palette.js
+++ 6/0606_bookpj/frontend/src/lib/palette.js
@@ -1,23 +1,28 @@
-:root {
-    --gray-0: #f8f9fa;
-    --gray-1: #f1f3f5;
-    --gray-2: #e9ecef;
-    --gray-3: #dee2e6;
-    --gray-4: #ced4da;
-    --gray-5: #adb5bd;
-    --gray-6: #868e96;
-    --gray-7: #495057;
-    --gray-8: #343a40;
-    --gray-9: #212529;
+const palette = {
+    gray: [
+        "#f8f9fa",
+        "#f1f3f5",
+        "#e9ecef",
+        "#dee2e6",
+        "#ced4da",
+        "#adb5bd",
+        "#868e96",
+        "#495057",
+        "#343a40",
+        "#212529",
+    ],
+    cyan: [
+        "#e3fafc",
+        "#c5f6fa",
+        "#99e9f2",
+        "#66d9e8",
+        "#3bc9db",
+        "#22b8cf",
+        "#15aabf",
+        "#1098ad",
+        "#0c8599",
+        "#0b7285",
+    ],
+};
 
-    --cyan-0: #e3fafc;
-    --cyan-1: #c5f6fa;
-    --cyan-2: #99e9f2;
-    --cyan-3: #66d9e8;
-    --cyan-4: #3bc9db;
-    --cyan-5: #22b8cf;
-    --cyan-6: #15aabf;
-    --cyan-7: #1098ad;
-    --cyan-8: #0c8599;
-    --cyan-9: #0b7285;
-}
+export default palette;
