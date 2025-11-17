d50375e00cd869b819283cb174f05d40b90d0bbd
Lacheln <psionict@naver.com>
2025-11-17 15:49:16 +0900

    onchange

--- 11/1117eventHandling/src/EventPractice.jsx
+++ 11/1117eventHandling/src/EventPractice.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 const EventPractice = () => {
+    const [message, setMessage] = useState("");
     return (
         <div>
             <h1>이벤트 연습</h1>
+            <h2>현재 입력된 값 : {message}</h2>
             <input
                 type="text"
                 name="message"
                 placeholder="아무거나 입력해보세요"
-                onChange={(e) => console.log(e.target.value)}
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
             />
         </div>
     );
