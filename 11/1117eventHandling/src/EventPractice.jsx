de287eb004fa84ed90bea5faa42ccbd31ccc005c
Lacheln <psionict@naver.com>
2025-11-17 16:13:24 +0900

    객체

--- 11/1117eventHandling/src/EventPractice.jsx
+++ 11/1117eventHandling/src/EventPractice.jsx
@@ -1,43 +1,46 @@
 import React, { useState } from "react";
 
 const EventPractice = () => {
-    const [message, setMessage] = useState("");
-    const [nickname, setNickname] = useState("");
+    const [form, setForm] = useState({
+        username: "",
+        message: "",
+    });
 
-    const confirmMessage = (e) => {
-        setMessage(e.target.value);
+    const { username, message } = form;
+    const onChange = (e) => {
+        const nextForm = {
+            ...form,
+            [e.target.name]: e.target.value,
+        };
+        setForm(nextForm);
     };
 
-    const resetMessage = () => {
-        setMessage("");
-        setNickname("");
-    };
-
-    const changeNickname = (e) => {
-        setNickname(e.target.value);
+    const enterEvent = (e) => {
+        if (e.key === "Enter") onChange(e);
     };
     return (
         <div>
             <h1>이벤트 연습</h1>
             <h2>현재 입력된 값 : {message}</h2>
-            <h2>닉네임 : {nickname} </h2>
+            <h2>닉네임 : {username} </h2>
             <input
                 type="text"
                 name="message"
                 placeholder="아무거나 입력해보세요"
                 value={message}
-                onChange={confirmMessage}
+                onChange={onChange}
             />
 
             <input
                 type="text"
-                name="nickname"
+                name="username"
                 placeholder="닉네임 입력"
-                value={nickname}
-                onChange={changeNickname}
+                value={username}
+                onChange={onChange}
+                onKeyDown={enterEvent}
             />
 
-            <button onClick={resetMessage}>초기화</button>
+            <button onClick={onChange}>확인</button>
         </div>
     );
 };
