15767c6f4aa6bd2c3f014475bb8987da476c4bf2
Lacheln <psionict@naver.com>
2025-11-20 00:55:56 +0900

    맵2

--- 11/1117ref/src/ValidationSample.jsx
+++ 11/1117ref/src/ValidationSample.jsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ValidationSample = () => {
-    const names = ["눈사람", "얼음", "눈", "바람"];
-    const nameList = names.map((name, id) => <li key={id}>{name}</li>);
-    return <div>{nameList}</div>;
+    const [names, setNames] = useState([
+        { id: 1, text: "눈사람" },
+        { id: 2, text: "얼음" },
+        { id: 3, text: "눈" },
+        { id: 4, text: "바람" },
+    ]);
+
+    const [inputText, setInputText] = useState("");
+    const [nextId, setNextId] = useState(5);
+
+    const onChange = (e) => setInputText(e.target.value);
+    const onClick = () => {
+        const nextNames = [
+            ...names,
+            {
+                id: nextId,
+                text: inputText,
+            },
+        ];
+        setNextId(nextId + 1);
+        setNames(nextNames);
+        setInputText("");
+    };
+
+    const onRemove = (id) => {
+        const nextNames = names.filter((name) => name.id !== id);
+        setNames(nextNames);
+    };
+    const namesList = names.map((name) => (
+        <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+            {name.text}
+        </li>
+    ));
+    return (
+        <div>
+            <input type="text" value={inputText} onChange={onChange} />
+            <div>{namesList}</div>
+            <button onClick={onClick}>클릭</button>
+        </div>
+    );
 };
 
 export default ValidationSample;
