004f8d570ce87b24b4024984bbdf3b512431666c
Lacheln <psionict@naver.com>
2025-11-20 00:24:21 +0900

    맵

--- 11/1117ref/src/ValidationSample.jsx
+++ 11/1117ref/src/ValidationSample.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ValidationSample = () => {
     const names = ["눈사람", "얼음", "눈", "바람"];
-    const nameList = names.map((name) => <li>{name}</li>);
+    const nameList = names.map((name, id) => <li key={id}>{name}</li>);
     return <div>{nameList}</div>;
 };
 
