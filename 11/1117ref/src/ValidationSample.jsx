984ac4f19f1d20f16b3667eaac14e34661655fb1
Lacheln <psionict@naver.com>
2025-11-19 17:28:22 +0900

    복습

--- 11/1117ref/src/ValidationSample.jsx
+++ 11/1117ref/src/ValidationSample.jsx
@@ -1,41 +1,9 @@
-import { Component } from "react";
-import "./ValidationSample.css";
+import React from "react";
 
-class ValidationSample extends Component {
-    state = {
-        password: "",
-        clicked: false,
-        validated: false,
-    };
-
-    handleChange = (e) => {
-        this.setState({
-            password: e.target.value,
-        });
-    };
-
-    handleButtonClick = () => {
-        this.setState({
-            clicked: true,
-            validated: this.state.password === "0000",
-        });
-    };
-
-    render() {
-        return (
-            <div>
-                <input
-                    type="password"
-                    value={this.state.password}
-                    onChange={this.handleChange}
-                    className={
-                        this.state.clicked ? (this.state.validated ? "success" : "failure") : ""
-                    }
-                />
-                <button onClick={this.handleButtonClick}>검증하기</button>
-            </div>
-        );
-    }
-}
+const ValidationSample = () => {
+    const names = ["눈사람", "얼음", "눈", "바람"];
+    const nameList = names.map((name) => <li>{name}</li>);
+    return <div>{nameList}</div>;
+};
 
 export default ValidationSample;
