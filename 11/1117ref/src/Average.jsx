98837d770e95a9e273b3840c33d57cd93103444e
Lacheln <psionict@naver.com>
2025-12-11 03:08:02 +0900

    useRef 공부

--- 11/1117ref/src/Average.jsx
+++ 11/1117ref/src/Average.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useState } from "react";
+import React, { useCallback, useMemo, useRef, useState } from "react";
 const getAverage = (numbers) => {
     console.log("평균값 계산중");
     if (numbers.length === 0) return 0;
@@ -9,21 +9,25 @@ const getAverage = (numbers) => {
 const Average = () => {
     const [list, setList] = useState([]);
     const [number, setNumber] = useState("");
+    const inputEl = useRef(null); // useRef 선언
 
     const onChange = useCallback((e) => {
         setNumber(e.target.value);
-    }, []);
+    }, []); //컴포넌트가 처음 렌더링될 때만 함수 생성
 
     const onInsert = useCallback(() => {
         const nextList = list.concat(parseInt(number));
         setList(nextList);
         setNumber("");
-    }, [list, number]);
+        inputEl.current.focus();
+    }, [list, number]); // number 혹은 list가 바뀌었을 때만 함수 생성
 
     const avg = useMemo(() => getAverage(list), [list]);
     return (
         <div>
-            <input type="text" value={number} onChange={onChange} />
+            {/* 해당 요소를 useRef로 만든 변수에 연결하겠다는 의미 */}
+            <input type="text" value={number} onChange={onChange} ref={inputEl} />
+
             <button onClick={onInsert}>등록</button>
             <ul>
                 {list.map((value, index) => (
